4dc9712265b247369af674188ec5c0bcb2bd18b3
emilstepanian <emilstepanian@hotmail.com>
2021-03-13 20:51:36 +0100

    Bump

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,7 @@
 import Screen from './Screen';
-
-export { Screen };
+import Text from './Text';
+import View from './View';
+import GradientButton from './Buttons/Gradient';
+import Button from './Buttons/Standard';
+import NavBackButton from './Buttons/NavBack';
+export { Screen, Text, View, Button, GradientButton, NavBackButton };
