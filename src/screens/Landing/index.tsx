fbf76d21fef6e8e44d09dbdbccbfbab8abf8ada9
emilstepanian <emilstepanian@hotmail.com>
2021-03-13 20:19:48 +0100

    initial setup of landing

--- src/screens/Landing/index.tsx
+++ src/screens/Landing/index.tsx
@@ -1,14 +1,38 @@
 import React from 'react';
-import { Text } from 'react-native';
-import { Screen } from '../../components';
+import { Screen, Text, View } from '../../components';
+import { StyleSheet } from 'react-native';
 
 const LandingScreen = () => {
   return (
-    <Screen>
-      <Text>Hej</Text>
-      <Text>Hej</Text>
+    <Screen style={styles.container}>
+      <View center middle>
+        <View row>
+          <Text h1 bold>
+            {'Hacker' + ' '}
+          </Text>
+          <Text h1 bold primary>
+            News
+          </Text>
+        </View>
+        <Text gray h3>
+          Sample App
+        </Text>
+      </View>
+      <View center middle>
+        <Text h2>Description goes here</Text>
+      </View>
+      <View center middle>
+        <Text h2>Description goes here</Text>
+      </View>
     </Screen>
   );
 };
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'space-around',
+  },
+});
 
 export default LandingScreen;
