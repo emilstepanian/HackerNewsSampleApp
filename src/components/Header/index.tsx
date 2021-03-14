818f798b60a24c25e10ed75ac8d8aebe9f6b69e3
emilstepanian <emilstepanian@hotmail.com>
2021-03-14 10:32:08 +0100

    Added header component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { StyleSheet } from 'react-native';
+import { Sizes } from '../../constants';
+import NavBackButton from '../Buttons/NavBack';
+import Text from '../Text';
+import View from '../View';
+
+interface Props {
+  title: string;
+  onNavBack?: () => void;
+}
+
+const Header = ({ title, onNavBack }: Props) => {
+  const headerTitle = (
+    <Text h1 bold>
+      {title}
+    </Text>
+  );
+
+  const navBack = onNavBack && <NavBackButton onNavBack={onNavBack} />;
+
+  return (
+    <View style={styles.container}>
+      <View>
+        {navBack}
+        {headerTitle}
+      </View>
+    </View>
+  );
+};
+export const styles = StyleSheet.create({
+  container: {
+    paddingTop: Sizes.base,
+    justifyContent: 'flex-start',
+  },
+});
+
+export default Header;
--- src/components/index.ts
+++ src/components/index.ts
@@ -4,4 +4,5 @@ import View from './View';
 import GradientButton from './Buttons/Gradient';
 import Button from './Buttons/Standard';
 import NavBackButton from './Buttons/NavBack';
-export { Screen, Text, View, Button, GradientButton, NavBackButton };
+import Header from './Header';
+export { Screen, Text, View, Button, GradientButton, NavBackButton, Header };
