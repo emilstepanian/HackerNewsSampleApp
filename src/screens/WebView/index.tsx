efb2bc199211690002889a397dad65cd13db6bb2
emilstepanian <emilstepanian@hotmail.com>
2021-03-14 19:16:08 +0100

    navback header for webview

--- src/screens/WebView/index.tsx
+++ src/screens/WebView/index.tsx
@@ -1,11 +1,10 @@
 import { useNavigation } from '@react-navigation/core';
 import React from 'react';
 import { StyleSheet } from 'react-native';
-import { Header, Loader, Screen, View } from '../../components';
-import { Sizes } from '../../constants';
-
 import { WebView } from 'react-native-webview';
 import { useSelector } from 'react-redux';
+import { Loader, NavBackButton, Screen, View } from '../../components';
+import { Sizes } from '../../constants';
 import { AppState } from '../../store/initialState';
 
 const ExternalWebView = () => {
@@ -17,7 +16,9 @@ const ExternalWebView = () => {
 
   return (
     <Screen style={styles.container}>
-      <Header title={''} onNavBack={() => navigation.goBack()} />
+      <View style={styles.navBack}>
+        <NavBackButton onNavBack={() => navigation.goBack()} />
+      </View>
       <WebView
         source={{ uri: selectedStory.url }}
         startInLoadingState={true}
@@ -35,6 +36,8 @@ export const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'space-around',
+  },
+  navBack: {
     paddingHorizontal: Sizes.padding,
   },
 });
