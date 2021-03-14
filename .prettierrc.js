400910f5d66f15937df4e9851909e8667f689c4a
emilstepanian <emilstepanian@hotmail.com>
2021-03-14 11:50:50 +0100

    updated trailingcomma prettier rule

--- .prettierrc.js
+++ .prettierrc.js
@@ -2,7 +2,7 @@ module.exports = {
   bracketSpacing: true,
   jsxBracketSameLine: true,
   singleQuote: true,
-  trailingComma: 'all',
+  trailingComma: 'es5',
   endOfLine: "lf",
   tabWidth: 2,
   semi: true
